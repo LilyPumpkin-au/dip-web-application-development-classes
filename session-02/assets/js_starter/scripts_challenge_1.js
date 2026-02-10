d2da46c83633f1f7c436cbc7f812742e32690674
LilyPumpkin-au <lily.pumpkin.au@gmail.com>
2026-02-10 23:32:55 +0800

    edit S2-challenge1

--- session-02/assets/js_starter/scripts_challenge_1.js
+++ session-02/assets/js_starter/scripts_challenge_1.js
@@ -1,11 +1,20 @@
 // Challenge 1 Starter
+
 // Initialise the array
 let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
 
 // Use the pop() method to remove the last element from the array
+numbers.pop();
+console.log(numbers);
 
-// Use the unshift() method to add the last element to the beginning of the array
+// Use the unshift() method adds new elements to the beginning of the array
+numbers.unshift(6, 7);
+console.log(numbers);
 
 // Use the slice() method to exclude the first 3 elements and return a new array
+numbers.slice(0, 2);
+console.log(numbers.slice(0, 2));
 
 // Set the value of the outputfield inner html to the new array
+document.getElementById("OutputField").innerHTML = numbers;
