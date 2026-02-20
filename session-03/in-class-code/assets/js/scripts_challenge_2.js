61e3d29edffd92ab003de562aa05f6b23a7f66ba
LilyPumpkin-au <lily.pumpkin.au@gmail.com>
2026-02-20 22:40:08 +0800

    feat: Session 3 challenge2 edit

--- session-03/in-class-code/assets/js/scripts_challenge_2.js
+++ session-03/in-class-code/assets/js/scripts_challenge_2.js
@@ -20,21 +20,37 @@ const userObject = {
 // Deep clone the complexObject three times using the spread operator for shallow cloning
 // const deepClone = structuredClone(userObject);
 
-// const copyUserObject1 = JSON.parse(JSON.stringify(userObject));
-// const copyUserObject2 = JSON.parse(JSON.stringify(userObject));
-// const copyUserObject3 = JSON.parse(JSON.stringify(userObject));
+// shallow copy
+// const copyUserObject1 = { ...userObject };
+// const copyUserObject2 = { ...userObject };
+// const copyUserObject3 = { ...userObject };
 
-const copyUserObject1 = { ...userObject };
-const copyUserObject2 = { ...userObject };
-const copyUserObject3 = { ...userObject };
-console.log(copyUserObject1);
+// Deep copy
+const copyUserObject1 = structuredClone(userObject);
+const copyUserObject2 = structuredClone(userObject);
+const copyUserObject3 = structuredClone(userObject);
+
+// shallowcopyでもプリミティブ型の１階層目は参照じゃなくて、コピーになるので、反映される
+copyUserObject1.age = 40;
+copyUserObject2.age = 25;
+copyUserObject3.age = 60;
+
+copyUserObject1.firstName = "Mike";
+copyUserObject2.firstName = "Michael";
+copyUserObject3.firstName = "Mickey";
+
+// これは、オブジェクトの中のオブジェクトなので、参照が入ってる。
+// だから、shallowcopyでは、copyUserObject1 だけをかえてるのに、オリジナルのオブジェクトも２も３も全部のアドレスのcityがTokyoに変わってしまう。
+// Deepcopy をすると、オリジナルも２も３もかわらない。このユーザの情報だけ変わる。
+copyUserObject1.address.city = "Tokyo";
 
 // Store the cloned objects in an array
-const copyUserObject1Array = Object.entries(copyUserObject1);
-console.log(copyUserObject1Array);
+const users = [copyUserObject1, copyUserObject2, copyUserObject3];
+console.log("users array :", users);
+console.log("user object original :", userObject);
 
 // Sort the array of cloned objects based on the 'age' property in ascending order
-const sortArray = copyUserObject1Array.sort((a, b) => (a.age > b.age ? 1 : -1));
+const sortArray = users.sort((a, b) => (a.age > b.age ? 1 : -1));
 
 // Log the sorted array to the console
 console.log(sortArray);
