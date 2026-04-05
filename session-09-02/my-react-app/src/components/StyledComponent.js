5e4ae05fa106493481faad8e895979d89282dcc5
LilyPumpkin-au <lily.pumpkin.au@gmail.com>
2026-04-05 23:15:12 +0800

    Session 9 Activitiy 3

--- session-09-02/my-react-app/src/components/StyledComponent.js
+++ session-09-02/my-react-app/src/components/StyledComponent.js
@@ -1,14 +1,23 @@
 // note this file needs to be moved into my-react-app to work
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "../asset/css/style.css";
 
-function StyledComponent() {
+export default function StyledComponent() {
+  // add useState() seconds, change the number to change the duration
+  const [animationDuration, setAnimationDuration] = useState(5);
+
+  // add useEffect
+  useEffect(() => {
+    console.log("Component mounted or animationDuration Changed:");
+  }, [animationDuration]);
+
   return (
-    <section className="animatedSection">
+    <section
+      className="animatedSection"
+      style={{ animationDuration: `${animationDuration}s` }}
+    >
       This is a styled section with animation
     </section>
   );
 }
-
-export default StyledComponent;
